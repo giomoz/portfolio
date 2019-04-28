ccd52e33b7fac431a3c58096180598340445b05e
giomoz <49450319+giomoz@users.noreply.github.com>
2019-04-28 14:53:54 +0200

    Delete Rabano project

--- js/projects-data.js
+++ js/projects-data.js
@@ -14,20 +14,14 @@ const projectsData = [
 		images: ['brafa_01.png', 'brafa_02.png', 'brafa_03.png', 'brafa_04.png'],
 		
 	},
-	{
-		ref: 'rabano',
-		color: { r: 185, g: 105, b: 143 },
-		title: 'Rabano',
-		introImage: 'dog.jpeg',
-		images: ['wall.jpeg', 'wall.jpeg'],
-		description: `n a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content.`,
-	},
+
 	{
 		ref: 'ronie',
 		color: { r: 255, g: 206, b: 174 },
 		title: 'Ronie',
 		introImage: 'ronie-copy.jpg',
 		images: ['ronie-copy.jpg', 'dog.jpeg'],
+		description: `n a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content.`,
 	},
 	{
 		ref: 'bye',
