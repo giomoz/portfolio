848c3ba337fca7d6f93191d537e7e09eb6ce74d1
Hayk An <say@hihayk.com>
2020-05-07 15:41:05 +0200

    Revert "Update projects-data.js"
    This reverts commit 1728d18a80493130e696e54e299f1da6fbc7025e.

--- js/projects-data.js
+++ js/projects-data.js
@@ -1,6 +1,7 @@
 const projectsData = [
 	{
 		ref: 'moritz',
+		color: { r: 254, g: 206, b: 0 },
 		title: 'Moritz',
 		introImage: 'moritz_portada.png',
 		images: ['moritz_01.png', 'moritz_02.png', 'moritz_03.png','moritz_04.png'],
@@ -8,6 +9,7 @@ const projectsData = [
 	},
 	{
 		ref: 'embarazo',
+		color: { r: 24, g: 204, b: 130 },
 		title: 'Mi embarazo <br>al día',
 		introImage: 'embarazo_portada.png',
 		images: ['embarazo_01.gif', 'embarazo_02.png', 'embarazo_03.png','embarazo_04.png','embarazo_05.png','embarazo_06.png','embarazo_07.png','embarazo_08.png'],
@@ -15,6 +17,7 @@ const projectsData = [
 	},
 	{
 		ref: 'brafa',
+		color: { r: 255, g: 52, b: 93 },
 		title: 'Brafa <br>B-Champion',
 		introImage: 'brafa_portada.png',
 		images: ['brafa_01.png', 'brafa_02.png', 'brafa_03.png', 'brafa_04.png'],
@@ -23,6 +26,7 @@ const projectsData = [
 	},
 	{
 		ref: 'vets',
+		color: { r: 11, g: 35, b: 63},
 		title: 'Vets & <br>Clinics',
 		introImage: 'vets_portada.png',
 		images: ['vets_01.png', 'vets_02.png', 'vets_03.png','vets_04.png','vets_05.png'],
@@ -31,6 +35,8 @@ const projectsData = [
 
 	{
 		ref: 'puratos',
+		color: { r: 239, g: 190, b: 38
+		      },
 		title: 'Puratos',
 		introImage: 'puratos_portada.png',
 		images: ['puratos_01.png', 'puratos_02.png', 'puratos_03.png','puratos_05.png','puratos_06.png'],
@@ -39,6 +45,7 @@ const projectsData = [
 	
 	{
 		ref: 'bye',
+		color: { r: 109, g: 185, b: 190 },
 		title: 'That&#8217s it, for now... <br>Thank you!',
 		introImage: 'bye.gif',
 	},
